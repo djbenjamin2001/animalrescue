1cfbdffc55d960a7683ba987779e13145af9100d
djbenjamin2001 <benjaminbatbayli@gmail.com>
2023-03-30 10:51:53 +0200

    added rest structure and updated docs

--- routes/animals.routes.js
+++ routes/animals.routes.js
@@ -2,10 +2,39 @@ const express = require("express")
 const router = express.Router()
 const Animal = require("../models/animals.models")
 const auth = require("../auth-middleware")
-router.get("/animals", async function(request, response, next){
+
+/*router.get("/animals", async function(request, response, next){
     try {
        let result =  await Animal.find();
        return response.status(200).json(result)
+    } catch (error) {
+       return next(error) 
+    }
+})*/ 
+
+//http://localhost:4000/api/v1/animals?offset=0&limit=5
+router.get("/animals", async function(request, response, next){
+    let offset = parseInt(request.query.offset) || 0
+    let limit = parseInt(request.query.limit) || 5
+    try {
+        let count =  (await Animal.find()).length
+       let results =  await Animal.find().skip(offset).limit(limit)
+       let queryStringNext = `?offset=${offset+limit}&limit=${limit}`
+       let queryStringPrevious = null; 
+       if (offset >= limit) {
+        queryStringPrevious = `?offset=${offset-limit}&limit=${limit}`
+       }
+       let apiUrl = `${request.protocol}://${request.hostname}${request.hostname === 'localhost' ? ":4000" : ''}`
+       let apiPath = `${request.baseUrl}${request.path}`
+       
+       let output ={
+        count,
+        next:(offset + limit < count) ? apiUrl + apiPath + queryStringNext : null,
+        previous: offset > 0 ? apiUrl + apiPath + queryStringPrevious : null,
+        results,
+        url: apiUrl + request.originalUrl
+       }
+       return response.status(200).json(output)
     } catch (error) {
        return next(error) 
     }
